68d6276cf7d7513c08019cac261c59706d0e5be5
Nadezhda <nadyakh00@gmail.com>
2022-12-16 08:58:05 +0900

    Change JDK version

--- restsimulateapp/src/main/java/khovanets/restsimulateapp/RestSimulateApp.java
+++ restsimulateapp/src/main/java/khovanets/restsimulateapp/RestSimulateApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestSimulateApp {
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestSimulateApp.class, args);
 	}
